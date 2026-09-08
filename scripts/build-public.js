26b2de36ec0be74309610423dbd8f3beccf1281b
nategluk <nateglukhov@gmail.com>
2026-09-07 19:43:56 -0500

    kidults

--- scripts/build-public.js
+++ scripts/build-public.js
@@ -40,6 +40,7 @@ const SITE_PAGES = [
   "documents/protocol-avd-312-17.html",
   "documents/protocol-media-integration.html",
   "documents/protocol-playground.html",
+  "documents/protocol-kidults.html",
   "documents/book-sweet-dream.html",
   "locations/detskiy-zhir-mall.html",
   "locations/dolphin-pool.html",
